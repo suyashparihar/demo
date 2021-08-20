d4690f2c62e383066291fa0bb3a84ea119c86996
suyashparihar <suyash.parihar@gmail.com>
2021-08-21 00:57:48 +0530

    Commit all changes

---
+++ Demo1_git/src/main/java/com/suyash/Demo1_git/Demo1GitApplication.java
@@ -0,0 +1,15 @@
+package com.suyash.Demo1_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1GitApplication.class, args);
+		
+		System.out.println("Hello Git");
+	}
+
+}
---
+++ Demo1_git/src/test/java/com/suyash/Demo1_git/Demo1GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suyash.Demo1_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
